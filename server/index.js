a81d5f6d9c10b91220d9293d3393848e57a64620
Elizabeth Arinder <emcclosky@gmail.com>
2018-06-04 12:23:55 -0400

    Added route handler for google OAuth

--- server/index.js
+++ server/index.js
@@ -9,8 +9,14 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
- }, (accessToken) => {
+  }, (accessToken) => {
     console.log(accessToken);
+  }
+ )
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
  })
 );
 
