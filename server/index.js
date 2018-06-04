2251e564d5cdf6609edc1ac5e733d1c1a5019733
Elizabeth Arinder <emcclosky@gmail.com>
2018-06-04 11:45:03 -0400

    First commit

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-//clientID 346441846289-b13s8inhfsu75mr5jqs0fu4q12e5idmu.apps.googleusercontent.com
-//clientSecret k7WmdQCMaahw8NYwVYDL53fY
 
 passport.use(new GoogleStrategy());
 
